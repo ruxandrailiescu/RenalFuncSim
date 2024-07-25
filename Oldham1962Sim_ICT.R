06d329edc52bf4053e46dbd409f5e8464c06e9d8
Ruxandra Iliescu <iliescuruxandra23@stud.ase.ro>
2024-07-25 19:42:32 +0300

    Oldham simulation -- feedback ICT

--- Oldham1962Sim_ICT.R
+++ Oldham1962Sim_ICT.R
@@ -12,21 +12,30 @@
 #------------------------------------------------------------------------------
 
 
-gendat <- function(N, mean1, sd1, mean2, sd2, k)
+gendat <- function(N, mean1, sd1, mean2, sd2, k, 
+                   type="corr")
 {
   correlation_coeffs <- numeric(k)
+  correlation_coeffsLR <- numeric(k)
   
   for (i in 1:k)
   {
     y0 <- rnorm(N, mean=mean1, sd=sd1)
     y1 <- rnorm(N, mean=mean2, sd=sd2)
-    
+
     correlation_coeffs[i] <- cor(y0, y1-y0)
+    
+    change <- y1-y0
+    scaled_change <- scale(change)
+    scaled_y0 <- scale(y0)
+    correlation_coeffsLR[i] <- coef(lm(scaled_change ~ scaled_y0))["scaled_y0"] 
+    
   }
   
-  summary(correlation_coeffs)
-  plot(density(correlation_coeffs))
-  return(correlation_coeffs)
+  summary(correlation_coeffs)                      ###>>> aceast output nu va fi afisat cand chemi functia (plot-ul de mai jos insa da)
+  plot(density(correlation_coeffs))                
+  if(type=="corr"){return(correlation_coeffs)}
+  if(type=="LR"){return(correlation_coeffsLR)}
 }
 
 
@@ -38,6 +47,11 @@ gendat <- function(N, mean1, sd1, mean2, sd2, k)
 
 set.seed(123)
 corr <- gendat(1000, 50, 2, 15, 2, 10000)
+
+set.seed(123) ###>>>acelasi seed ca sa am exact aceleasi date generate la fiecare iteratie. difera doar modul de calcul al corelatiei
+LR <- gendat(1000, 50, 2, 15, 2, 10000,type="LR")
+
+summary(corr-LR)
 
 #------------------------------------------------------------------------------
 # Scenario 2: dissimilar variances
